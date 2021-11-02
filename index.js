b36d4f6a0f9aa4193a71609244a79c400d68ff8f
honeychuckles <kaylaedwards575@gmail.com>
2021-11-02 15:11:28 +0000

    lord have mercy

--- index.js
+++ index.js
@@ -1,27 +1,69 @@
 //1
-function areBothSame(){
+function areBothSame(int1,int2){
+    if (int1 == int2){
+        return true;
+    }else{
+        return false;
+    }
   
 }
+//console.log(areBothSame(true, 1)); // returns true
+//console.log(areBothSame("2", 2)) // returns true
+//console.log(areBothSame("5", 2)); // returns false
 
 //2
-function areBothEqual(){
+function areBothEqual(int1, int2){
+    if(int1===int2){
+        return true;
+    }else{
+        return false;
+    }
   
 }
+//console.log(areBothEqual("2", "2")); // returns true
+//console.log(areBothEqual("2", 2)) // returns false 
+//console.log(areBothEqual(true, 1)); // returns false 
+//console.log(areBothEqual(7, 7));// returns true  
 
 //3
-function areAllFourEqual(){
+function areAllFourEqual(int1,int2,int3,int4){
+    if(int1 === int2 && int3 === int4 && int1 === int3){
+        return true;
+    }else{
+        return false;
+    }
   
 }
-
+console.log(areAllFourEqual("2", "2", "2", "2")); // returns true
+console.log(areAllFourEqual("2", "2", "2", 2)); // returns false 
 
 //4
-function areAllFourSame(){
+function areAllFourSame(int1,int2,int3,int4){
+    if(int1 == int2 && int3 == int4 && int3 == int1){
+        return true
+    }else{
+        return false
+    }
   
 }
+//console.log(areAllFourSame("2", "2", "2", "2")) // returns true
+//console.log(areAllFourSame("2", "2", "2", 2)) // returns true 
+//console.log(areAllFourSame(true, true, 1, "1")) // returns true
 
 //5
-function areAllFourSameOrEqual(){
-  
+function areAllFourSameOrEqual(int1,int2,int3,int4){
+    if((int1 === (int2 && int3 &&int4))){
+        return 'equal'
+    }else if((int1 == (int2&& int3 && int4))) {
+        return 'same'
+    }else{
+        return 'null'
+    }
+    
 }
+
+areAllFourSame("2", "2", "2", "2") // returns "equal"
+//areAllFourSame(true, true, 1, "1") // returns "same"
+//areAllFourSame(true, false, 1, "1") // returns null
 
 
